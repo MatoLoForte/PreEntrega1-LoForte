2ada5394d075e792438532bedc8c40c55eb20f88
Martin Lo Forte <martinloforte@gmail.com>
2023-01-09 15:16:54 -0300

    corrección en rango de meses - horoscopo

--- js/script.js
+++ js/script.js
@@ -88,7 +88,7 @@ function zodiaco(dia, mes) {
             signo = "Acuario";
         }
     } else if (mes === 2) {
-        if (dia < 21) {
+        if (dia < 20) {
             signo = "Acuario";
         } else {
             signo = "Piscis";
@@ -100,13 +100,13 @@ function zodiaco(dia, mes) {
             signo = "Aries";
         }
     } else if (mes === 4) {
-        if (dia < 21) {
+        if (dia < 20) {
             signo = "Aries";
         } else {
             signo = "Tauro";
         }
     } else if (mes === 5) {
-        if (dia < 23) {
+        if (dia < 21) {
             signo = "Tauro";
         } else {
             signo = "Géminis";
@@ -118,37 +118,37 @@ function zodiaco(dia, mes) {
             signo = "Cáncer";
         }
     } else if (mes === 7) {
-        if (dia < 21) {
+        if (dia < 23) {
             signo = "Cáncer";
         } else {
             signo = "Leo";
         }
     } else if (mes === 8) {
-        if (dia < 21) {
+        if (dia < 23) {
             signo = "Leo";
         } else {
             signo = "Virgo";
         }
     } else if (mes === 9) {
-        if (dia < 21) {
+        if (dia < 23) {
             signo = "Virgo";
         } else {
             signo = "Libra";
         }
     } else if (mes === 10) {
-        if (dia < 21) {
+        if (dia < 23) {
             signo = "Libra";
         } else {
             signo = "Escorpio";
         }
     } else if (mes === 11) {
-        if (dia < 21) {
+        if (dia < 22) {
             signo = "Escorpio";
         } else {
             signo = "Sagitario";
         }
     } else if (mes === 12) {
-        if (dia < 23) {
+        if (dia < 22) {
             signo = "Sagitario";
         } else {
             signo = "Capricornio";
@@ -163,39 +163,39 @@ function horoscopo(signo) {
     let horoscopo = "";
 
     if (signo === "Aries") {
-        horoscopo ="Horóscopo de hoy:\n Iniciarás tu jornada a todo motor con muchas actividades planeadas para hoy. No te pases del límite.\nAmor:\n Te será imposible no ceder ante la insistencia de tu pareja para pasar la jornada de hoy fuera del hogar. Disfrútala al máximo.\nRiqueza:\n Tendrás una jornada bastante tranquila a nivel laboral en el día de hoy. Aprovéchala para adelantar cuanto trabajo puedas.\nBienestar:\n No caigas en el carpe diem a nivel económico o podrás llegar a tener graves consecuencias en un futuro cercano. Planifica en detalle.";
+        horoscopo ="HOROSCOPO DE HOY:\n Iniciarás tu jornada a todo motor con muchas actividades planeadas para hoy. No te pases del límite.\nAMOR:\n Te será imposible no ceder ante la insistencia de tu pareja para pasar la jornada de hoy fuera del hogar. Disfrútala al máximo.\nRIQUEZA:\n Tendrás una jornada bastante tranquila a nivel laboral en el día de hoy. Aprovéchala para adelantar cuanto trabajo puedas.\nBIENESTAR:\n No caigas en el carpe diem a nivel económico o podrás llegar a tener graves consecuencias en un futuro cercano. Planifica en detalle.";
     
     } else if (signo === "Tauro") {
-        horoscopo ="Horóscopo de hoy:\n La diosa de la fortuna te sonreirá el día de hoy. Muéstrate confiado a la hora de iniciar proyectos de toda índole en lo personal.\nAmor:\n No busques perfección total a la hora de encontrar una pareja. Te será imposible llenar cada expectativa que tengas.\nRiqueza:\n Cuentas con las herramientas para sobresalir, pero el cansancio extremo y las frustraciones están opacando tu presencia.\nBienestar:\n Tendrás una jornada llena de traspiés y dificultades. Esto te presentará un problema pues estás acostumbrado a lo fácil y a tener éxito siempre.";
+        horoscopo ="HOROSCOPO DE HOY:\n La diosa de la fortuna te sonreirá el día de hoy. Muéstrate confiado a la hora de iniciar proyectos de toda índole en lo personal.\nAMOR:\n No busques perfección total a la hora de encontrar una pareja. Te será imposible llenar cada expectativa que tengas.\nRIQUEZA:\n Cuentas con las herramientas para sobresalir, pero el cansancio extremo y las frustraciones están opacando tu presencia.\nBIENESTAR:\n Tendrás una jornada llena de traspiés y dificultades. Esto te presentará un problema pues estás acostumbrado a lo fácil y a tener éxito siempre.";
 
     } else if (signo === "Géminis") {
-        horoscopo ="Horóscopo de hoy:\n El cambio de actitud frente a la vida puede servir para quitarte malestares de encima. Renuévate encontrando otro camino.\nAmor:\n Estás pasando por un momento de soledad a punto de terminar. Prepárate con la mejor predisposición para conocer el amor.\nRiqueza:\n Sé realista en las metas que te impones, el no alcanzarlas provoca frustraciones innecesarias que pueden frenar tu desempeño.\nBienestar:\n Tu resistencia natural te ayudará a luchar contra enfermedades y afecciones. Lograrás gozar de una salud sin altibajos y equilibrada.";
+        horoscopo ="HOROSCOPO DE HOY:\n El cambio de actitud frente a la vida puede servir para quitarte malestares de encima. Renuévate encontrando otro camino.\nAMOR:\n Estás pasando por un momento de soledad a punto de terminar. Prepárate con la mejor predisposición para conocer el amor.\nRIQUEZA:\n Sé realista en las metas que te impones, el no alcanzarlas provoca frustraciones innecesarias que pueden frenar tu desempeño.\nBIENESTAR:\n Tu resistencia natural te ayudará a luchar contra enfermedades y afecciones. Lograrás gozar de una salud sin altibajos y equilibrada.";
 
     } else if (signo === "Cáncer") {
-        horoscopo = "Horóscopo de hoy:\n Los reencuentros y los festejos entre parientes serán oportunos en este momento, donde no todo es color de rosa en el núcleo familiar.\nAmor:\n El trabajo y la rutina están trayendo problemas en tu pareja, no permitas que esto pase. Acércate y demuéstrale tu amor.\nRiqueza:\n La adaptabilidad es una cualidad de gran importancia a la hora de nuevos emprendimientos. Sé proactivo al cambio.\nBienestar:\n Busca el bienestar en las cosas simples de la vida. La familia, los amigos y los seres queridos te lo brindarán con el corazón y sin retaceos.";
+        horoscopo = "HOROSCOPO DE HOY:\n Los reencuentros y los festejos entre parientes serán oportunos en este momento, donde no todo es color de rosa en el núcleo familiar.\nAMOR:\n El trabajo y la rutina están trayendo problemas en tu pareja, no permitas que esto pase. Acércate y demuéstrale tu amor.\nRIQUEZA:\n La adaptabilidad es una cualidad de gran importancia a la hora de nuevos emprendimientos. Sé proactivo al cambio.\nBIENESTAR:\n Busca el bienestar en las cosas simples de la vida. La familia, los amigos y los seres queridos te lo brindarán con el corazón y sin retaceos.";
     } else if (signo === "Leo") {
-        horoscopo = "Horóscopo de hoy:\n Lograrás entender, no a través de las mejores experiencias, lo importante de proyectar una imagen socialmente aceptable.\nAmor:\n Te sentirás completamente incomprendido debido a ciertas actitudes por parte de tu pareja en el día de hoy. Demuéstralo.\nRiqueza:\n Preocúpate de tus asuntos y evita ser arrastrado a grupos laborales que sabes no traerán nada positivo a tu carrera.\nBienestar:\n No permitas ser sobrepasado por ningún tipo de situación. Asegúrate de mantener la mente serena y lograrás salir de cualquier aprieto.";
+        horoscopo = "HOROSCOPO DE HOY:\n Lograrás entender, no a través de las mejores experiencias, lo importante de proyectar una imagen socialmente aceptable.\nAMOR:\n Te sentirás completamente incomprendido debido a ciertas actitudes por parte de tu pareja en el día de hoy. Demuéstralo.\nRIQUEZA:\n Preocúpate de tus asuntos y evita ser arrastrado a grupos laborales que sabes no traerán nada positivo a tu carrera.\nBIENESTAR:\n No permitas ser sobrepasado por ningún tipo de situación. Asegúrate de mantener la mente serena y lograrás salir de cualquier aprieto.";
 
     } else if (signo === "Virgo") {
-        horoscopo = "Horóscopo de hoy:\n Evita sacar a relucir viejos errores solo para demostrar que tienes razón en las discusiones con amigos de antaño.\nAmor:\n Te sorprenderán gratamente ciertas actitudes por parte de tu pareja. Notarás que nunca llegas a conocer a alguien completamente.\nRiqueza:\n No temas tomar el crédito que te has ganado por tu esfuerzo y dedicación. No permitas que se abusen de tu tranquilidad.\nBienestar:\n Tu capacidad de escoger las palabras adecuadamente te será de increíble ayuda en el ámbito laboral. Procura utilizarla en cada ocasión que tengas.";
+        horoscopo = "HOROSCOPO DE HOY:\n Evita sacar a relucir viejos errores solo para demostrar que tienes razón en las discusiones con amigos de antaño.\nAMOR:\n Te sorprenderán gratamente ciertas actitudes por parte de tu pareja. Notarás que nunca llegas a conocer a alguien completamente.\nRIQUEZA:\n No temas tomar el crédito que te has ganado por tu esfuerzo y dedicación. No permitas que se abusen de tu tranquilidad.\nBIENESTAR:\n Tu capacidad de escoger las palabras adecuadamente te será de increíble ayuda en el ámbito laboral. Procura utilizarla en cada ocasión que tengas.";
 
     } else if (signo === "Libra") {
-        horoscopo = "Horóscopo de hoy:\n No contarás con el apoyo de los astros durante la jornada de hoy. Evita tomar decisiones a largo plazo, o medítalas en detalle.\nAmor:\n La vida no siempre da lugar para sueños de amor perfecto. Deberás aprender que la tolerancia es el secreto del éxito.\nRiqueza:\n Debes aprender a usar tus habilidades en tu beneficio de mejor manera. Conócete a fondo y lograrás maravillas.\nBienestar:\n El estrés y el cansancio de una rutina agotadora acabarán por provocar un paro total en tus actividades cuando tu cuerpo diga basta. Precaución.";
+        horoscopo = "HOROSCOPO DE HOY:\n No contarás con el apoyo de los astros durante la jornada de hoy. Evita tomar decisiones a largo plazo, o medítalas en detalle.\nAMOR:\n La vida no siempre da lugar para sueños de amor perfecto. Deberás aprender que la tolerancia es el secreto del éxito.\nRIQUEZA:\n Debes aprender a usar tus habilidades en tu beneficio de mejor manera. Conócete a fondo y lograrás maravillas.\nBIENESTAR:\n El estrés y el cansancio de una rutina agotadora acabarán por provocar un paro total en tus actividades cuando tu cuerpo diga basta. Precaución.";
 
     } else if (signo === "Escorpio") {
-        horoscopo = "Horóscopo de hoy:\n Finalmente será tu turno de enfrentar las responsabilidades por acciones tuyas del pasado. Que el orgullo no se interponga.\nAmor:\n No a todas las personas les agrada que les digan que hacer a cada paso del camino. Dale a tu pareja la libertad que necesita.\nRiqueza:\n Aprende a compartir tus conocimientos con aquellos pares laborales con los que tengas afinidad. Esto mejorará tu entorno.\nBienestar:\n No cedas ante la presión de encajar correctamente en la sociedad, renunciando a tu personalidad única e irrepetible. Desarrolla tu personalidad.";
+        horoscopo = "HOROSCOPO DE HOY:\n Finalmente será tu turno de enfrentar las responsabilidades por acciones tuyas del pasado. Que el orgullo no se interponga.\nAMOR:\n No a todas las personas les agrada que les digan que hacer a cada paso del camino. Dale a tu pareja la libertad que necesita.\nRIQUEZA:\n Aprende a compartir tus conocimientos con aquellos pares laborales con los que tengas afinidad. Esto mejorará tu entorno.\nBIENESTAR:\n No cedas ante la presión de encajar correctamente en la sociedad, renunciando a tu personalidad única e irrepetible. Desarrolla tu personalidad.";
 
     } else if (signo === "Sagitario") {
-        horoscopo = "Horóscopo de hoy:\n Estarás propenso a vivir vicisitudes durante el día de hoy. Chequea todo dos veces y no dejes nada librado al azar.\nAmor:\n La familia no es algo que uno pueda elegir, así que no culpes a tu pareja por los defectos de su entorno familiar.\nRiqueza:\n Limítate en los gastos superfluos e innecesarios, porque no siempre puedes darte los gustos. Aprende a ser más ahorrativo.\nBienestar:\n Investiga detenidamente el lugar en el cual depositarás tus ahorros. Evalúa todas las posibilidades y sólo entonces decide.";
+        horoscopo = "HOROSCOPO DE HOY:\n Estarás propenso a vivir vicisitudes durante el día de hoy. Chequea todo dos veces y no dejes nada librado al azar.\nAMOR:\n La familia no es algo que uno pueda elegir, así que no culpes a tu pareja por los defectos de su entorno familiar.\nRIQUEZA:\n Limítate en los gastos superfluos e innecesarios, porque no siempre puedes darte los gustos. Aprende a ser más ahorrativo.\nBIENESTAR:\n Investiga detenidamente el lugar en el cual depositarás tus ahorros. Evalúa todas las posibilidades y sólo entonces decide.";
 
     } else if (signo === "Capricornio") {
-        horoscopo = "Horóscopo de hoy:\n No intentes ser tú el que desarrolle y abarque todas las tareas del hogar. Haz notar las responsabilidades de cada uno.\nAmor:\n Tu pasividad en la relación está haciendo que tu pareja pierda el respeto hacia ti. Deberás procurar poner un alto a esto.\nRiqueza:\n Deberás tomar una determinación, o modificas tu manera de trabajar o cambias de trabajo. No puedes continuar así.\nBienestar:\n No permitas que los impulsos tomen control de ti y te haga actuar tontamente. Debes aprender a dominarte más efectivamente o tendrás problemas.";
+        horoscopo = "HOROSCOPO DE HOY:\n No intentes ser tú el que desarrolle y abarque todas las tareas del hogar. Haz notar las responsabilidades de cada uno.\nAMOR:\n Tu pasividad en la relación está haciendo que tu pareja pierda el respeto hacia ti. Deberás procurar poner un alto a esto.\nRIQUEZA:\n Deberás tomar una determinación, o modificas tu manera de trabajar o cambias de trabajo. No puedes continuar así.\nBIENESTAR:\n No permitas que los impulsos tomen control de ti y te haga actuar tontamente. Debes aprender a dominarte más efectivamente o tendrás problemas.";
 
     } else if (signo === "Acuario") {
-        horoscopo = "Horóscopo de hoy:\n No podrás alcanzar tus metas de la vida sentado en tu sillón. Utiliza el día de hoy para replantearte tus conductas de vida.\nAmor:\n Discusiones esporádicas vivirás hoy junto a tu pareja. Simplemente parecerán sintonizar frecuencias diferentes. Paciencia.\nRiqueza:\n No habrá vuelta atrás de las determinaciones que pretendes tomar en el día de hoy. Piensa dos veces antes de hacerlo.\nBienestar:\n No subestimes el valor de ser invaluable para tus superiores. Solo así tendrás grandes posibilidades de no ser afectado por reformas laborales.";
+        horoscopo = "HOROSCOPO DE HOY:\n No podrás alcanzar tus metas de la vida sentado en tu sillón. Utiliza el día de hoy para replantearte tus conductas de vida.\nAMOR:\n Discusiones esporádicas vivirás hoy junto a tu pareja. Simplemente parecerán sintonizar frecuencias diferentes. Paciencia.\nRIQUEZA:\n No habrá vuelta atrás de las determinaciones que pretendes tomar en el día de hoy. Piensa dos veces antes de hacerlo.\nBIENESTAR:\n No subestimes el valor de ser invaluable para tus superiores. Solo así tendrás grandes posibilidades de no ser afectado por reformas laborales.";
 
     } else {
-        horoscopo = "Horóscopo de hoy:\n Deberás pasar por un momento muy difícil el día de hoy. Afortunadamente podrás contar con el apoyo incondicional de seres amados.\nAmor:\n Son pocas las opciones que tu pareja te está dejando a la hora de tomar una determinación acerca del destino de la relación.\nRiqueza:\n La posibilidad de montar el negocio propio nunca estuvo tan cerca. No cejes, continua hacia delante con voluntad de acero.\nBienestar:\n Busca el momento adecuado para poner ciertos temas en la pareja sobre la mesa. No siempre podrás contar con una buena predisposición.";
+        horoscopo = "HOROSCOPO DE HOY:\n Deberás pasar por un momento muy difícil el día de hoy. Afortunadamente podrás contar con el apoyo incondicional de seres amados.\nAMOR:\n Son pocas las opciones que tu pareja te está dejando a la hora de tomar una determinación acerca del destino de la relación.\nRIQUEZA:\n La posibilidad de montar el negocio propio nunca estuvo tan cerca. No cejes, continua hacia delante con voluntad de acero.\nBIENESTAR:\n Busca el momento adecuado para poner ciertos temas en la pareja sobre la mesa. No siempre podrás contar con una buena predisposición.";
     }
 
     return horoscopo;
